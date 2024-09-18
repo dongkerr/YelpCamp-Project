b4387b66260abf49ed7c78021ddf565d31a2c5bf
Dongkerr <dongkerr.wang@gmail.com>
2024-09-19 00:55:46 +0200

    Adding in Authentication

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ const session = require("express-session")
 const flash = require("connect-flash")
 const ExpressError = require("./utils/ExpressError")
 const methodoverride = require("method-override")
+const passport = require("passport")
+const LocalStrategy = require("passport-local")
+const User = require("./models/user")
 
-const campgrounds = require("./routes/campgrounds")
-const reviews = require("./routes/reviews")
+const campgroundRoutes = require("./routes/campgrounds")
+const reviewRoutes = require("./routes/reviews")
+const userRoutes = require("./routes/users")
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
 const db = mongoose.connection;
@@ -28,26 +32,44 @@ app.use(methodoverride("_method"))
 app.use(express.static(path.join(__dirname, "public")))
 
 const sessionConfig = {
-    secret: "thisshouldbeabettersecret!",
-    resave:false,
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
     saveUninitialized: true,
-    cookie:{
+    cookie: {
         httpOnly: true,
-        expires: Date.now() + 1000*60*60*24*7,
-        maxAge: 1000*60*60*24*7
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
+
 app.use(session(sessionConfig))
-app.use(flash())
+app.use(flash());
+
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
 
 app.use((req,res,next)=>{
-    res.locals.success = req.flash("success");
-    res.locals.error = req.flash("error")
-    next()
+    console.log(req.session)
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    res.locals.currentUser = req.user;
+    next();
 })
 
-app.use("/campgrounds", campgrounds)
-app.use("/campgrounds/:id/reviews", reviews)
+app.get("/fakeUser", async(req,res)=>{
+    const user = new User({email: "keke@gmail.com", username:"keke"})
+    const newUser = await User.register(user, "chicken")
+    res.send(newUser)
+})
+
+app.use("/campgrounds", campgroundRoutes)
+app.use("/campgrounds/:id/reviews", reviewRoutes)
+app.use("/", userRoutes)
 
 app.get("/",(req,res)=>{
     res.render("home")
