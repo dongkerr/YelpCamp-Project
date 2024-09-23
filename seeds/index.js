70d5ee006da9f94338e31ca1f844669c6c7619d7
Dongkerr <dongkerr.wang@gmail.com>
2024-09-23 15:27:39 +0200

    Basic Authorization

--- seeds/index.js
+++ seeds/index.js
@@ -18,6 +18,7 @@ const seedDB = async()=>{
         const random1000 = Math.floor(Math.random()*1000);
         const price = Math.floor(Math.random()*20)+10;
         const camp = new Campground({
+            author: "66f15b9b95d34fa47c9418b8",
             location:`${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: `https://picsum.photos/400?random=${Math.random()}`,
