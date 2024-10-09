e6046dde961ed861b7c0460a86194c4ad01502b6
Dongkerr <dongkerr.wang@gmail.com>
2024-10-09 15:15:26 +0200

    Common Security

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-if(process.env.NODE_ENV !== "production"){
-    require("dotenv").config()
-}
+// if(process.env.NODE_ENV !== "production"){
+//     require("dotenv").config()
+// }
+require("dotenv").config()
 
 console.log(process.env.SECRET)
 console.log(process.env.API_KEY)
@@ -16,6 +17,9 @@ const methodoverride = require("method-override")
 const passport = require("passport")
 const LocalStrategy = require("passport-local")
 const User = require("./models/user")
+const helmet = require("helmet")
+
+const mongoSanitize = require('express-mongo-sanitize');
 
 const campgroundRoutes = require("./routes/campgrounds")
 const reviewRoutes = require("./routes/reviews")
@@ -37,13 +41,16 @@ app.set("views", path.join(__dirname, "views"))
 app.use(express.urlencoded({extended: true}))
 app.use(methodoverride("_method"))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(mongoSanitize());
 
 const sessionConfig = {
+    name:"kk-session",
     secret: 'thisshouldbeabettersecret!',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure:true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
@@ -51,6 +58,51 @@ const sessionConfig = {
 
 app.use(session(sessionConfig))
 app.use(flash());
+app.use(helmet());
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: [],
+        connectSrc: ["'self'", ...connectSrcUrls],
+        scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+        styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+        workerSrc: ["'self'", "blob:"],
+        objectSrc: [],
+        imgSrc: [
+            "'self'",
+            "blob:",
+            "data:",
+            "https://res.cloudinary.com/dk0voghbf/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+            "https://images.unsplash.com/",
+        ],
+        fontSrc: ["'self'", ...fontSrcUrls],
+    },
+}))
 
 app.use(passport.initialize())
 app.use(passport.session())
@@ -61,7 +113,6 @@ passport.deserializeUser(User.deserializeUser())
 
 
 app.use((req,res,next)=>{
-    console.log(req.session)
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     res.locals.currentUser = req.user;
