bf2882abc5eb52395c1590c6d1c7276abba18be4
Dongkerr <dongkerr.wang@gmail.com>
2024-10-10 10:52:25 +0200

    reday to deploy

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,10 +1,10 @@
 
-const goodCampground = JSON.parse(campground)
+// const goodCampground = JSON.parse(campground)
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/light-v10', // style URL
-    center: goodCampground.geometry.coordinates, // starting position [lng, lat]
+    center: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 9, // starting zoom
 });
 
@@ -12,11 +12,11 @@ map.addControl(new mapboxgl.NavigationControl());
 
 //https://docs.mapbox.com/mapbox-gl-js/example/marker-from-geocode/
 new mapboxgl.Marker()
-    .setLngLat(goodCampground.geometry.coordinates)
+    .setLngLat(campground.geometry.coordinates)
     .setPopup(
         new mapboxgl.Popup({ offset: 25 })
             .setHTML(
-                `<h3>${goodCampground.title}</h3><p>${goodCampground.location}</p>`
+                `<h3>${campground.title}</h3><p>${campground.location}</p>`
             )
     )
     .addTo(map);
